db322a1f8e9f6486de3917895d5ef3ed1dbf63b6
Fredrik Welander <welandfr@arcada.fi>
2024-01-15 19:36:36 +0200

    tagged template literal

--- api/foo.js
+++ api/foo.js
@@ -9,3 +9,20 @@ export default async function handler(req, res) {
     return res.status(500).json({ error });
   }
 }
+
+/**
+ *  tagged template literal
+ *
+
+function tagFun(foo, ...vals) {
+    console.log('f2', foo, vals)
+    // note ... Spread operator syntax skapar här en ny array av alla resterande argument
+}
+
+const fname = 'Fredrik'
+const lname = 'Welander'
+
+tagFun(`hello ${fname} ${lname}!`)
+
+tagFun`hello ${fname} ${lname}!`
+ */
